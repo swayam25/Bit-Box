d1b2aff8e12f742997174104c90b7dd30d9ff19a
swayam25 <178068815+swayam25@users.noreply.github.com>
2024-10-29 20:31:42 +0530

    Update editor styling and line numbers

--- client/src/lib/Editor.svelte
+++ client/src/lib/Editor.svelte
@@ -14,10 +14,10 @@
 
 <div class="h-[90.2vh] w-screen pt-8 pb-[68px] md:pb-[0.1px] pl-[35px] md:pl-[95px] pr-[35px]">
     <div class="relative h-full w-full">
-        <textarea bind:value={code} class="{themeColor} text-[20px] pl-8 focus:outline-none font-mono h-full w-full rounded-md resize-none absolute top-0 left-0" {disabled} />
-        <div class="absolute top-0 left-0 h-full w-full pointer-events-none">
+        <textarea bind:value={code} class="{themeColor} text-[20px] pl-10 pt-2 focus:outline-none font-mono h-full w-full rounded-md resize-none absolute top-0 left-0" {disabled} />
+        <div class="absolute top-0 left-0 pl-4 pt-2 h-full w-full pointer-events-none">
             {#each code.split("\n") as _, i}
-                <div class="text-left px-2 text-gray-500">{i + 1}</div>
+                <div class="text-left text-gray-500">{i + 1}</div>
             {/each}
         </div>
     </div>
