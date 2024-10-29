41541841d16bb135ddf4ec7fb09e03d831a35d6f
swayam25 <178068815+swayam25@users.noreply.github.com>
2024-10-29 21:11:33 +0530

    Fix editor

--- client/src/lib/Editor.svelte
+++ client/src/lib/Editor.svelte
@@ -2,6 +2,7 @@
     export let code, theme, disabled;
 
     let themeColor = "";
+    let lineNumbersDiv;
 
     $: {
         if(theme == "dark") {
@@ -10,15 +11,23 @@
             themeColor = "bg-white text-orange-rust";
         }
     }
+
+    function handleScroll(event) {
+        lineNumbersDiv.scrollTop = event.target.scrollTop;
+    }
 </script>
 
 <div class="h-[90.2vh] w-screen pt-8 pb-[68px] md:pb-[0.1px] pl-[35px] md:pl-[95px] pr-[35px]">
     <div class="relative h-full w-full">
-        <textarea bind:value={code} class="{themeColor} text-[20px] pl-10 pt-2 focus:outline-none font-mono h-full w-full rounded-md resize-none absolute top-0 left-0" {disabled} />
-        <div class="absolute top-0 left-0 pl-4 pt-2 h-full w-full pointer-events-none">
-            {#each code.split("\n") as _, i}
-                <div class="text-left text-gray-500">{i + 1}</div>
-            {/each}
+        <div class="relative h-full w-full overflow-hidden {themeColor} rounded-md top-0 left-0 flex flex-row-reverse gap-x-2 p-5">
+            <textarea bind:value={code} class="text-[20px] bg-transparent focus:outline-none font-mono h-full w-full resize-none overflow-auto text-nowrap" {disabled} on:scroll={handleScroll} />
+            <div class="h-full pointer-events-none overflow-hidden" bind:this={lineNumbersDiv} style="width: {`${String(code.split("\n").length).length + 1}ch`}">
+                <div class="h-full w-full">
+                    {#each code.split("\n") as _, i}
+                        <p class="text-left text-gray-500 overflow-hidden">{i + 1}</p>
+                    {/each}
+                </div>
+            </div>
         </div>
     </div>
 </div>
